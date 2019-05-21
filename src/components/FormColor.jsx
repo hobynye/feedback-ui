280017905ebad819032f4b8f1c42852277d38f60
Marc Billow <mbillow@indeed.com>
2019-05-20 20:29:17 -0500

    Recapitialize the Colors

--- src/components/FormColor.jsx
+++ src/components/FormColor.jsx
@@ -12,17 +12,17 @@ export default class FormColor extends Component {
                     <label className="btn btn-success color-button active"
                            style={{"width": "33.33%"}} onClick={() => {this.props.handleColorChange("green")}} >
                         <Form.Check type="radio" name="color" id="green" autoComplete="off"
-                                    label="Green" value="green" defaultChecked/>
+                                    label="Green" value="Green" defaultChecked/>
                     </label>
                     <label className="btn btn-warning color-button"
                            style={{"width": "33.33%"}} onClick={() => {this.props.handleColorChange("yellow")}}>
                         <Form.Check type="radio" name="color" id="yellow" autoComplete="off"
-                                    label="Yellow" value="yellow" />
+                                    label="Yellow" value="Yellow" />
                     </label>
                     <label className="btn btn-danger color-button"
                            style={{"width": "33.33%"}} onClick={() => {this.props.handleColorChange("red")}}>
                         <Form.Check type="radio" name="color" id="red" autoComplete="off"
-                                    label="Red" value="red"/>
+                                    label="Red" value="Red"/>
                     </label>
                 </ButtonGroup>
             </Form.Group>
