d10ddacc4dcaf66aa9c2736e50d46bfc9f66d0fa
Marc Billow <mbillow@indeed.com>
2019-05-23 10:34:19 -0500

    Add user list and delete functionality

--- src/components/ManageUsers.jsx
+++ src/components/ManageUsers.jsx
@@ -1,26 +1,13 @@
 import React, {Component} from 'react';
+import Gravatar from 'react-gravatar';
 import Navigation from './Navigation';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Table from 'react-bootstrap/Table';
-
-const UserRow = ({userList}) => (
-    <>
-        {userList.map((user, i) => (
-            <tr id={"userList" + user.id} key={i}>
-                <td>{user.name}</td>
-                <td>{user.title}</td>
-                <td><a href={"#users/edit/" + user.id}>Edit</a> <a href='#users' onClick={() => {deleteUser(user.id)}}>Delete</a></td>
-            </tr>
-        ))}
-    </>
-);
-
-function deleteUser(id) {
-    console.log(id);
-}
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 
 export default class ManageUsers extends Component {
@@ -29,9 +16,13 @@ export default class ManageUsers extends Component {
         this.state = {
             user: "",
             loading: true,
-            userList: []
-        }
-
+            userList: [],
+            show: false,
+            modalUser: {}
+        };
+        this.showDeleteModal = this.showDeleteModal.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        this.deleteUser = this.deleteUser.bind(this);
     }
 
     componentWillMount() {
@@ -57,7 +48,6 @@ export default class ManageUsers extends Component {
             fetch("/api/admin/users")
                 .then(response => response.json())
                 .then((jsonData) => {
-                    console.log(jsonData["users"]);
                     this.setState({loading: false, userList: jsonData["users"]});
                 })
                 .catch((error) => {
@@ -66,6 +56,33 @@ export default class ManageUsers extends Component {
                 });
         })
     };
+
+    handleClose() {
+        this.setState({show: false});
+    }
+
+    deleteUser(id) {
+        fetch('/api/admin/user/' + id, {
+            method: 'DELETE'
+        })
+            .then(() => {
+                    this.setState({
+                        show: false
+                    });
+                    document.getElementById("userList" + id).style.display = "none";
+                }
+            )
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
+    showDeleteModal(user) {
+        this.setState({
+            modalUser: user,
+            show: true
+        })
+    }
 
     render() {
         return (
@@ -80,12 +97,51 @@ export default class ManageUsers extends Component {
                                         <thead>
                                         <tr>
                                             <th>Name</th>
-                                            <th>Title</th>
+                                            <th className={"d-none d-lg-table-cell"}>Title</th>
                                             <th>Actions</th>
                                         </tr>
                                         </thead>
                                         <tbody>
-                                        <UserRow userList={this.state.userList}/>
+                                        {this.state.userList.map((user, i) => (
+                                            <tr id={"userList" + user.id} key={i}>
+                                                <td>
+                                                    <div style={{display: "inline-block"}}>
+                                                        <Gravatar email={user.email} size={35}
+                                                                  style={{
+                                                                      borderRadius: "50%",
+                                                                      verticalAlign: "sub",
+                                                                      marginRight: "10px"
+                                                                  }}/>
+                                                    </div>
+                                                    <div style={{display: "inline-block"}}>
+                                                        <strong><p style={{marginBottom: "0px"}}>{user.name}</p>
+                                                        </strong>
+                                                        <p style={{display: "block", color: "#6c757d"}}>{user.email}</p>
+                                                    </div>
+                                                </td>
+                                                <td className={"d-none d-lg-table-cell"}>
+                                                    <p style={{marginBottom: "0px"}}>{user.title}</p>
+                                                    <p style={{
+                                                        display: "block",
+                                                        color: "#6c757d"
+                                                    }}>{user.color} {user.letter}</p>
+                                                </td>
+                                                <td>
+                                                    <p style={{marginBottom: "0px"}}>
+                                                        <a href={"#users/edit/" + user.id}>
+                                                            <i className="fas fa-edit"></i> Edit
+                                                        </a>
+                                                    </p>
+                                                    <p>
+                                                        <a href='#users' onClick={() => {
+                                                            this.showDeleteModal(user)
+                                                        }} className={"text-danger"}>
+                                                            <i className="fas fa-trash-alt"></i> Delete
+                                                        </a>
+                                                    </p>
+                                                </td>
+                                            </tr>
+                                        ))}
                                         </tbody>
                                     </Table>
 
@@ -94,6 +150,19 @@ export default class ManageUsers extends Component {
                         </Col>
                     </Row>
                 </Container>
+                <Modal show={this.state.show} onHide={this.handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Delete {this.state.modalUser.name}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Are you sure you want to delete this user?</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="danger" onClick={() => {
+                            this.deleteUser(this.state.modalUser.id)
+                        }}>
+                            Delete User
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
             </div>
         )
     }
